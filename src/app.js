5659d1ec8b10a42d4b60375cf0bfc847d9440c11
Jan Dubniak <narusteras@gmail.com>
2018-05-29 00:57:15 +0200

    +all tests for components

--- src/app.js
+++ src/app.js
@@ -11,27 +11,27 @@ import './styles/styles.scss';
 import "react-dates/lib/css/_datepicker.css";
 
 const store = configureStore();
-
-store.dispatch(addExpense({
-    description: "Water Bill",
-    amount: 500,
-}));
-store.dispatch(addExpense({
-    description: "Gas Bill",
-    createdAt: 1000,
-}));
-store.dispatch(addExpense({
-    description: "Rent",
-    amount: 109500,
-}));
-
-console.log(getVisibleExpenses(store.getState().expenses, store.getState().filters));
-// store.dispatch(setTextFilter("water"));
-// setTimeout(()=>{
-//     store.dispatch(setTextFilter("bill"));
-// },3000);
-console.log(getVisibleExpenses(store.getState().expenses, store.getState().filters));
-
+//
+// store.dispatch(addExpense({
+//     description: "Water Bill",
+//     amount: 500,
+// }));
+// store.dispatch(addExpense({
+//     description: "Gas Bill",
+//     createdAt: 1000,
+// }));
+// store.dispatch(addExpense({
+//     description: "Rent",
+//     amount: 109500,
+// }));
+//
+// console.log(getVisibleExpenses(store.getState().expenses, store.getState().filters));
+// // store.dispatch(setTextFilter("water"));
+// // setTimeout(()=>{
+// //     store.dispatch(setTextFilter("bill"));
+// // },3000);
+// console.log(getVisibleExpenses(store.getState().expenses, store.getState().filters));
+// console.log('test');
 const jsx = (
     <Provider store={store}>
         <AppRouter />
