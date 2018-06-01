b72fdf1bffb6505d43bc65d7d2182adcb178d037
Jan Dubniak <narusteras@gmail.com>
2018-06-02 00:47:13 +0200

    src/app + Login/Logout watcher
    src/actions/auth + Google login logic
    + startLogin()
    + startLogout()
    
    src/components/Header
    + Logout button
    + Connection to store
    
    src/tests/components/Header
    +button tests
    
    src/components/Login + Google login view
    + Login Button
    + Connection to store
    
    src/tests/components/Login
    + Render test
    + Button onClick test
    
    src/firebase/firebase
    + googleAuthProvider provider for google api login
    + googleAuthProvider export
    
    src/router/AppRouter
    * / dir now leads to LoginPage
    + /dashboard leads to ExpenseDashboardPage

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 
-import './firebase/firebase';
+import {firebase} from './firebase/firebase';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import "react-dates/lib/css/_datepicker.css";
@@ -22,4 +22,13 @@ ReactDOM.render(<p>Loading...</p>, document.getElementById("app"));
 
 store.dispatch(startSetExpenses()).then(() => {
     ReactDOM.render(jsx, document.getElementById("app"));
+});
+
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        console.log('log in');
+    }
+    else {
+        console.log('log out')
+    }
 });
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -6,13 +6,16 @@ import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import Header from '../components/Header';
 import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
+import Login from '../components/Login';
+
 
 const AppRouter = () => (
     <BrowserRouter>
         <div>
             <Header/>
             <Switch>
-                <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+                <Route path="/" component={Login} exact={true}/>
+                <Route path="/dashboard" component={ExpenseDashboardPage}/>
                 <Route path="/create" component={AddExpensePage} />
                 <Route path="/edit/:id" component={EditExpensePage} />
                 <Route path="/help" component={HelpPage} />
